60db3ff290fc251df6f07610f18fbf0440119ae8
Kalyan Parajuli <kalyanranjanp@gmail.com>
2022-07-03 23:56:20 -0400

    Added all dates

--- src/python/scrape_all_calendar_data.py
+++ src/python/scrape_all_calendar_data.py
@@ -1,7 +1,9 @@
 import requests
 import time
 
-for year in range(1992, 2081):
+# Total available 1992-2080
+
+for year in range(2064, 2081):
     for month in range(1, 13):
         url = "https://nepalicalendar.rat32.com?selYear={yr}&selMonth={mth}&viewCalander=Show".format(yr=year, mth=month)
         filename = "calendar-{yr}y{mth}m.html".format(yr=year, mth=month)
